26ded5cca4a4021c718da25af05e43ff8452f84a
Saqib Ansari <saqib.ns111@gmail.com>
2023-04-07 07:02:19 +0500

    buid successfully with user id saved in mobile device

--- screens/SplashScreen.jsx
+++ screens/SplashScreen.jsx
@@ -2,29 +2,29 @@ import React, { useEffect } from "react";
 import { View, Image, StyleSheet } from "react-native";
 import { useNavigation } from "@react-navigation/native";
 import { COLORS, icons } from "../constants";
-// import AsyncStorage from "@react-native-async-storage/async-storage";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
 const SplashScreen = () => {
   const navigation = useNavigation();
 
   useEffect(() => {
     const timeoutId = setTimeout(() => {
-      navigation.navigate("Onboarding1");
+      checkIfLoggedIn();
     }, 3000);
 
     return () => clearTimeout(timeoutId);
   }, []);
 
-  // const checkIfLoggedIn = async () => {
-  //   const authId = await AsyncStorage.getItem("authId");
-  //   if (authId) {
-  //     // User is already authenticated, navigate to home screen
-  //     navigation.navigate("Home");
-  //   } else {
-  //     // User is not authenticated, navigate to sign up screen
-  //     navigation.navigate("Onboarding1");
-  //   }
-  // };
+  const checkIfLoggedIn = async () => {
+    const authId = await AsyncStorage.getItem("authId");
+    if (authId) {
+      // User is already authenticated, navigate to home screen
+      navigation.navigate("Home");
+    } else {
+      // User is not authenticated, navigate to sign up screen
+      navigation.navigate("Onboarding1");
+    }
+  };
 
   return (
     <View style={styles.container}>
